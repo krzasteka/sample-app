dc57e104fcde81ded9da30000265311e6984e83e
Andrzej Krzastek <krzasteka@evotech.com>
2015-02-21 08:13:05 -0500

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    @title = "sign up"
+  end 
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+  
+  def create
+      @user = User.create(user_params) 
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        @title = "Sign up"
+        render 'new'
+      end 
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
